474a90ce416654ad5fc804a542a2a1e0bcbde864
IAmAlmeida <joao.paulo.almeidaaa@gmail.com>
2018-10-11 09:50:04 +0100

    nand2tetris

--- IDW/Lab01/www/js/code.js
+++ IDW/Lab01/www/js/code.js
@@ -3,14 +3,12 @@ function somadig(num){
 
     do{
         somadig += num%10;
-        console.log(num+"--");
-        console.log(somadig);
         num = Math.floor(num/10,0);
         
     }while(num!=0);
     document.getElementById("result").innerHTML = "O resultado é: " + somadig;
 }
-function testenter(e,num){
+function testenter(e,num){ 
     var code = (e.keyCode ? e.keyCode : e.which);
     if(code == 13) {
         somadig(num);
