df9cd22d4736ef4dfd384399c8ecfc829d77f21e
João Almeida <joao.paulo.almeidaaa@gmail.com>
2018-12-15 18:04:07 +0000

    yey

--- AFP/lab13/Exe6/main.c
+++ AFP/lab13/Exe6/main.c
@@ -10,13 +10,14 @@ void soma_media (int *v, int *soma, float *media){
 
 int main()
 {
-    int i=0,soma=0,v1=0;
+    int i=0,soma=0,v1[10]={0};
     float media=0;
     puts("Introduza 10 valores para fazer a media");
     for(i=0;i<10;i++){
-        scanf("%d",&v1);
-        soma_media(&v1,&soma,&media);
+        scanf("%d",&v1[i]);
+        soma_media(&v1[i],&soma,&media);
     }
+
     printf("\n\n\n\n--------------\n%.2f",media);
 
     return 0;
